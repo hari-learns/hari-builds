90208ba4b04177c6ec4613535f6ec435bcb5fdd2
hari-learns <hariharanrp.dev@gmail.com>
2025-12-16 18:19:45 +0530

    check

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="relative z-10 text-center pb-20">
             <div className="footer-content flex flex-col items-center justify-center gap-2">
                 <p>Designed & Built by Hariharan R P</p>
-                <p className="footer-note">Built with Next.js & Tailwind CSS</p>
+                <p className="footer-note">Built with Next.js & ailwind CSS</p>
             </div>
         </footer>
     );
